ab83642863fba9df6341c85631d3672982353d95
s0je <cuentasegurajss@gmail.com>
2022-03-01 01:03:36 +0100

    Angular Uploade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "registro", component: RegistroComponent, children: [
+                                                              {path:'', component: FormularioRegistroComponent}]},
+  {path: "home", component:HomeComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
